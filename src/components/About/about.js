6a065d8e3b16a575b7a6aeb55dc17261455daccb
erlan-urazov <erlan.u@1fit.app>
2023-11-09 10:41:15 +0600

    initial commit

---
+++ src/components/About/about.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import './prices.css';
+
+
+const Prices = () => {
+    return (
+        <div className="flex">
+            <p className="text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid asperiores commodi debitis dicta distinctio dolorem doloremque error facere ipsam ipsum mollitia obcaecati perspiciatis praesentium, quidem quod repellat totam, vitae?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid asperiores commodi debitis dicta distinctio dolorem doloremque error facere ipsam ipsum mollitia obcaecati perspiciatis praesentium, quidem quod repellat totam, vitae?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid asperiores commodi debitis dicta distinctio dolorem doloremque error facere ipsam ipsum mollitia obcaecati perspiciatis praesentium, quidem quod repellat totam, vitae?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid asperiores commodi debitis dicta distinctio dolorem doloremque error facere ipsam ipsum mollitia obcaecati perspiciatis praesentium, quidem quod repellat totam, vitae?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid asperiores commodi debitis dicta distinctio dolorem doloremque error facere ipsam ipsum mollitia obcaecati perspiciatis praesentium, quidem quod repellat totam, vitae?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid asperiores commodi debitis dicta distinctio dolorem doloremque error facere ipsam ipsum mollitia obcaecati perspiciatis praesentium, quidem quod repellat totam, vitae?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid asperiores commodi debitis dicta distinctio dolorem doloremque error facere ipsam ipsum mollitia obcaecati perspiciatis praesentium, quidem quod repellat totam, vitae?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid asperiores commodi debitis dicta distinctio dolorem doloremque error facere ipsam ipsum mollitia obcaecati perspiciatis praesentium, quidem quod repellat totam, vitae?
+            </p>
+            <div className="w-300 h-300 bg-black"></div>
+        </div>
+    )
+}
+
+export default Prices;
